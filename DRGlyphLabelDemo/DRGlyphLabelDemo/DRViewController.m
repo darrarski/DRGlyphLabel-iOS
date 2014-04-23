bdef35ff6e73703341927d4a922d107bb0484be7
Darrarski <darrarski@gmail.com>
2014-04-23 16:23:07 +0200

    Demo - simple counter

--- DRGlyphLabelDemo/DRGlyphLabelDemo/DRViewController.m
+++ DRGlyphLabelDemo/DRGlyphLabelDemo/DRViewController.m
@@ -12,6 +12,8 @@
 
 @interface DRViewController ()
 
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, assign) NSUInteger counter;
 @property (nonatomic, strong) DRGlyphLabel *label;
 
 @end
@@ -24,10 +26,28 @@
 	
 	self.label = [[DRGlyphLabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
 	self.label.font = [[DRGlyphFont alloc] initWithName:@"font1"];
-	self.label.text = @"1234";
-	self.label.backgroundColor = [UIColor colorWithRed:0.683 green:0.902 blue:0.722 alpha:1.000];
-	
 	[self.view addSubview:self.label];
+	[self updateCounterLabel];
+	
+	self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1f
+                                                  target:self
+                                                selector:@selector(timerTick)
+                                                userInfo:nil
+                                                 repeats:YES];
+	
+}
+
+- (void)timerTick
+{
+	self.counter++;
+	[self updateCounterLabel];
+}
+
+- (void)updateCounterLabel
+{
+	self.label.text = [NSString stringWithFormat:@"%d", self.counter];
+	[self.label sizeToFit];
+	self.label.center = self.view.center;
 }
 
 @end
