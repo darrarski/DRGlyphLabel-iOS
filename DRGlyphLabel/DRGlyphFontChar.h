ad717179f3cea0f30b63f5322117a6c1b76ecf61
Darrarski <darrarski@gmail.com>
2014-04-23 17:18:40 +0200

    DRGlyphFontChar class

---
+++ DRGlyphLabel/DRGlyphFontChar.h
@@ -0,0 +1,24 @@
+//
+//  DRGlyphFontChar.h
+//  DRGlyphLabel
+//
+//  Created by Dariusz Rybicki on 23.04.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DRGlyphFontChar : NSObject
+
+@property (nonatomic, strong) NSString *filename;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat posX;
+@property (nonatomic, assign) CGFloat posY;
+@property (nonatomic, assign) CGFloat offsetX;
+@property (nonatomic, assign) CGFloat offsetY;
+@property (nonatomic, assign) CGFloat advanceX;
+
+@property (nonatomic, readonly) UIImage *image;
+
+@end
---
+++ DRGlyphLabel/DRGlyphFontChar.m
@@ -0,0 +1,38 @@
+//
+//  DRGlyphFontChar.m
+//  DRGlyphLabel
+//
+//  Created by Dariusz Rybicki on 23.04.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import "DRGlyphFontChar.h"
+
+@interface DRGlyphFontChar ()
+
+@property (nonatomic, readonly) CGRect rect;
+
+@end
+
+@implementation DRGlyphFontChar
+
+- (UIImage *)image
+{
+	UIImage *pageImage = [UIImage imageNamed:self.filename];
+    CGImageRef pageImageRef = CGImageCreateWithImageInRect([pageImage CGImage], self.rect);
+    UIImage *characterImage = [UIImage imageWithCGImage:pageImageRef
+												  scale:pageImage.scale
+											orientation:pageImage.imageOrientation];
+    CGImageRelease(pageImageRef);
+    return characterImage;
+}
+
+- (CGRect)rect
+{
+	return CGRectMake(self.posX,
+					  self.posY,
+					  self.width,
+					  self.height);
+}
+
+@end
