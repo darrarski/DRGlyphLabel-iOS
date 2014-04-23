462f4c370df6d9da4dcb2199dfc17968f17ca3d1
Darrarski <darrarski@gmail.com>
2014-04-23 12:03:19 +0200

    Initial commit, empty demo project files

---
+++ DRGlyphLabelDemo/DRGlyphLabelDemo/DRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DRAppDelegate.h
+//  DRGlyphLabelDemo
+//
+//  Created by Dariusz Rybicki on 23.04.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DRGlyphLabelDemo/DRGlyphLabelDemo/DRViewController.h
@@ -0,0 +1,13 @@
+//
+//  DRViewController.h
+//  DRGlyphLabelDemo
+//
+//  Created by Dariusz Rybicki on 23.04.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRViewController : UIViewController
+
+@end
